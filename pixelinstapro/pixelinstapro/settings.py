2d1f0edcf086320dc23e83e4018f1fb6263b4a30
kalidasan <kalidasan3489@gmail.com>
2026-03-07 08:10:52 -0800

    Update mobile layouts and add bottom navigation to templates

--- pixelinstapro/pixelinstapro/settings.py
+++ pixelinstapro/pixelinstapro/settings.py
@@ -79,15 +79,8 @@ WSGI_APPLICATION = 'pixelinstapro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pixelinsta',     
-        'USER': 'root',            
-        'PASSWORD': 'kali@2005',           
-        'HOST': '127.0.0.1',      
-        'PORT': '3306',           
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
